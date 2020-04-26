8cb2dc8309c67452869fcde981a2b63d069d8e05
bartbutenaers <bart.butenaers@gmail.com>
2020-04-26 22:33:08 +0200

    contributor

--- voice2json_config.js
+++ voice2json_config.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 Bart Butenaers
+ * Copyright 2020 Bart Butenaers & Johannes Kropf
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
