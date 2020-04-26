a58236f5af0cfd46e373a6e1af2123ea96143438
bartbutenaers <bart.butenaers@gmail.com>
2020-04-26 09:00:53 +0200

    Initial commit

---
+++ voice2json_training.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2020 Bart Butenaers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+ module.exports = function(RED) {
+    var settings = RED.settings;
+    
+    function Voice2JsonTrainingNode(config) {
+        RED.nodes.createNode(this, config);
+        
+        var node = this;
+        
+        // Retrieve the config node
+        node.voice2JsonConfig = RED.nodes.getNode(config.voice2JsonConfig);
+        
+        if (node.voice2JsonConfig) {
+        }
+
+        node.on("input", function(msg) {  
+        });
+        
+        node.on("close",function() { 
+        });
+    }
+    RED.nodes.registerType("voice2json-training", Voice2JsonTrainingNode);
+}
