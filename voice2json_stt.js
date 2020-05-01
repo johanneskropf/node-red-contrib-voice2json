4b7e1042242910320d7d7ef752b5d35521df06c6
johanneskropf <46578064+johanneskropf@users.noreply.github.com>
2020-05-01 10:31:12 +0200

    Reworked the node status function
    previous timeout always gets cleared also on close

--- voice2json_stt.js
+++ voice2json_stt.js
@@ -26,15 +26,20 @@
         this.outputField = config.outputField;
         this.profilePath = ""; //todo add check for length at execution
         this.validPath = false;
-      
-        function node_error_status(errtext){
-            node.status({fill:"red",shape:"dot",text:errtext});
-            setTimeout(() => {
-                node.status({});
-            },1500);
-        }
-      
+        this.statusTimer = false;
         var node = this;
+      
+        function node_status(text,color,shape,time){
+            node.status({fill:color,shape:shape,text:text});
+            if(node.statusTimer !== false){
+                clearTimeout(node.statusTimer);
+                node.statusTimer = false;
+            }
+            node.statusTimer = setTimeout(() => {
+                node.status({});
+                node.statusTimer = false;
+            },time);
+        }
 
         // Retrieve the config node
         node.voice2JsonConfig = RED.nodes.getNode(config.voice2JsonConfig);
@@ -60,7 +65,7 @@
             
             if(!node.validPath){
                 node.error("Profile path doesn't exist. Please check the profile path");
-                node_error_status("profile path error");
+                node_status("profile path error","red","dot",1500);
                 return;
             }
                             
@@ -71,13 +76,13 @@
                 } 
                 catch(err) {
                     node.error("Error getting file path from msg." + node.inputField + " : " + err.message);
-                    node_error_status("couldn't get file path from msg");
+                    node_status("couldn't get file path from msg","red","dot",1500);
                     return;
                 }
                 
                 if (!filePath || filePath === "" || typeof filePath !== 'string') {
                     node.error("The msg." + node.inputField + " should contain a file path");
-                    node_error_status("file path format is not valid");
+                    node_status("file path format is not valid","red","dot",1500);
                     return;
                 }
             }
@@ -87,11 +92,11 @@
 
             if (!fs.existsSync(filePath)){
                 node.error("The file path does not exist");
-                node_error_status("file path does not exist");
+                node_status("file path does not exist","red","dot",1500);
                 return;
             }
             
-            node.status({fill:"blue",shape:"dot",text:"working..."});
+            node_status("processing...","blue","dot",15000);
             
             const voice2json = "voice2json --profile " + node.profilePath + " transcribe-wav " + filePath;
             
@@ -100,18 +105,14 @@
                 
                 delete node.childProcess;
                 
-                setTimeout(() => {
-                    node.status({});
-                },1500);
-                
                 if (error) {
                     node.error(error.message);
-                    node.status({fill:"red",shape:"dot",text:"error"});
+                    node_status("error","red","dot",1500);
                     return;
                 }
                 if (stderr) {
                     node.error(stderr);
-                    node.status({fill:"red",shape:"dot",text:"stderr:error"});
+                    node_status("stderr:error","red","dot",1500);
                     return;
                 }
                 
@@ -120,6 +121,7 @@
                 }
                 catch(error) {
                     node.error("Error parsing json output : " + error.message);
+                    node_status("error parsing json","red","dot",1500);
                     return;
                 }
                 
@@ -128,17 +130,22 @@
                     RED.util.setMessageProperty(msg, node.outputField, outputValue, true);
                 } catch(err) {
                     node.error("Error setting value in msg." + node.outputField + " : " + err.message);
+                    node_status("error","red","dot",1500);
                     return;
                 }
             
                 node.send(msg);
-                node.status({fill:"green",shape:"dot",text:"success"});
+                node_status("success","green","dot",1500);
                 return;
             });
         });
         
         node.on("close",function() {
-            node.status({});
+            if(node.statusTimer !== false){
+               clearTimeout(node.statusTimer);
+               node.statusTimer = false;
+               node.status({});
+            }
             if(node.childProcess) {
                 node.childProcess.kill();
                 delete node.childProcess;
