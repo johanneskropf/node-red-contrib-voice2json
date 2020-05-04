73fd46981d2f91a15b82937546fe527f45ce7372
johanneskropf <46578064+johanneskropf@users.noreply.github.com>
2020-05-04 17:40:04 +0200

    added barebones edit and save on deploy functionality to the config

--- voice2json_config.js
+++ voice2json_config.js
@@ -15,16 +15,46 @@
  **/
  module.exports = function(RED) {
     var settings = RED.settings;
+    const fs = require("fs");
+    let sentencesSave = ""; //initiated as empty so we know if it changed
     
     function Voice2JsonConfigNode(config) {
         RED.nodes.createNode(this, config);
         this.profilePath = config.profilePath;
         
         var node = this;
+       
+        //bare bones save to file implementation if sentences.ini was loaded
+        node.sentencesFilePath = node.profilePath + "/sentences.ini";
+        if(sentencesSave.length !== 0){
+	        try{
+	            fs.writeFileSync(node.sentencesFilePath, sentencesSave);
+	        } catch(err){
+	            console.log('couldn\'t write to file: ' + err);
+	        }
+	    }
+        sentencesSave = ""; //reset after deploy
         
         node.on('close', function(){
 		});
     }
     
     RED.nodes.registerType("voice2json-config", Voice2JsonConfigNode);
+    
+    //load the file from fs
+    RED.httpAdmin.get("/voice2json-config/loadSentences", RED.auth.needsPermission('voice2json-config.read'), function(req, res){
+        let fileContent = fs.readFileSync(req.query.profilePath + "/sentences.ini", 'utf8');
+        res.json(fileContent);
+    });
+    
+    //get the internal copy if config gets opened again before redeploy
+    RED.httpAdmin.get("/voice2json-config/getSentences", RED.auth.needsPermission('voice2json-config.read'), function(req, res){
+        res.json(sentencesSave);
+    });
+    
+    //save an internal copy of the edits made in the config
+    RED.httpAdmin.get("/voice2json-config/saveSentences", RED.auth.needsPermission('voice2json-config.read'), function(req, res){
+        sentencesSave = req.query.sentences;
+        res.json("success");
+    });
 }
