8187865d67775969175c8782a10c6412110a6afc
johanneskropf <46578064+johanneskropf@users.noreply.github.com>
2020-05-09 11:23:40 +0200

    complete rework to use spawn instead of exec

--- voice2json_stt.js
+++ voice2json_stt.js
@@ -17,6 +17,7 @@
  module.exports = function(RED) {
     var settings = RED.settings;
     const { exec } = require("child_process");
+    const { spawn } = require("child_process");
     const fs = require("fs");
     
     function Voice2JsonSpeechToTextNode(config) {
@@ -27,6 +28,8 @@
         this.profilePath = ""; //todo add check for length at execution
         this.validPath = false;
         this.statusTimer = false;
+        this.statusTimer2 = false;
+        this.processingNow = false;
         var node = this;
       
         function node_status(text,color,shape,time){
@@ -40,94 +43,59 @@
                 node.statusTimer = false;
             },time);
         }
-
-        // Retrieve the config node
-        node.voice2JsonConfig = RED.nodes.getNode(config.voice2JsonConfig);
         
-        if (node.voice2JsonConfig) {
-            // Use the profile path which has been specified in the config node
-            node.profilePath = node.voice2JsonConfig.profilePath;
-            //check path
-            if (fs.existsSync(node.profilePath)){
-                node.validPath = true;
+        function node_status2(text,color,shape,time){
+            if(node.statusTimer2 !== false){
+                clearTimeout(node.statusTimer2);
+                node.statusTimer2 = false;
             }
+            node.statusTimer2 = setTimeout(() => {
+                node.status({fill:color,shape:shape,text:text});
+                node.statusTimer2 = false;
+            },time);
         }
-
-        node.on("input", function(msg) {
-            let filePath;
+        
+        function spawnTranscribe(msg){
+            try{
+                node.transcribeWav = spawn("voice2json",["--profile",node.profilePath,"transcribe-wav","--stdin-file"]);
+            } 
+            catch (error) {
+                node_status2("error strating","red","ring",1);
+                node.error(error);
+            }
             
-            if(node.childProcess) {
-                let warnmsg = "Ignoring input message because the previous message is not processed yet";
-                console.log(warnmsg);
-                node.warn(warnmsg);
+            node_status2("running","blue","ring",1);
+            
+            node.transcribeWav.stderr.on('data', (data)=>{
+                node.processingNow = false;
+                node.error(data.toString());
+                node_status("error","red","dot",1500);
+                node_status2("stopped","grey","ring",1500);
                 return;
-            }
+            });
             
-            if(!node.validPath){
-                node.error("Profile path doesn't exist. Please check the profile path");
-                node_status("profile path error","red","dot",1500);
-                return;
-            }
-                            
-            if (node.inputType === "msg") {
-                try {
-                    // Get the file path from the specified message field
-                    filePath = RED.util.getMessageProperty(msg, node.inputField);
-                } 
-                catch(err) {
-                    node.error("Error getting file path from msg." + node.inputField + " : " + err.message);
-                    node_status("couldn't get file path from msg","red","dot",1500);
-                    return;
-                }
-                
-                if (!filePath || filePath === "" || typeof filePath !== 'string') {
-                    node.error("The msg." + node.inputField + " should contain a file path");
-                    node_status("file path format is not valid","red","dot",1500);
-                    return;
-                }
-            }
-            else { // str
-                filePath = node.inputField;
-            }
-
-            if (!fs.existsSync(filePath)){
-                node.error("The file path does not exist");
-                node_status("file path does not exist","red","dot",1500);
-                return;
-            }
+            node.transcribeWav.on('close', function (code,signal) {
+                node_status2("stopped","grey","ring",1);
+            });
             
-            node_status("processing...","blue","dot",15000);
+            node.transcribeWav.stdout.on('data', (data)=>{
             
-            const voice2json = "voice2json --profile " + node.profilePath + " transcribe-wav " + filePath;
-            
-            node.childProcess = exec(voice2json, (error, stdout, stderr) => {
-                let outputValue;
-                
-                delete node.childProcess;
-                
-                if (error) {
-                    node.error(error.message);
-                    node_status("error","red","dot",1500);
-                    return;
-                }
-                if (stderr) {
-                    node.error(stderr);
-                    node_status("stderr:error","red","dot",1500);
-                    return;
-                }
+                node.processingNow = false;
+                node.transcription = data.toString();
                 
                 try {
-                    outputValue = JSON.parse(stdout);
+                    node.outputValue = JSON.parse(node.transcription);
                 }
                 catch(error) {
                     node.error("Error parsing json output : " + error.message);
                     node_status("error parsing json","red","dot",1500);
+                    node_status2("running","blue","ring",1500);
                     return;
                 }
                 
                 try {
                     // Set the converted value in the specified message field (of the original input message)
-                    RED.util.setMessageProperty(msg, node.outputField, outputValue, true);
+                    RED.util.setMessageProperty(msg, node.outputField, node.outputValue, true);
                 } catch(err) {
                     node.error("Error setting value in msg." + node.outputField + " : " + err.message);
                     node_status("error","red","dot",1500);
@@ -136,8 +104,99 @@
             
                 node.send(msg);
                 node_status("success","green","dot",1500);
+                node_status2("running","blue","ring",1500);
                 return;
             });
+            return;
+            
+        }
+        
+        function writeStdin(msg){
+            
+            node_status("processing...","blue","dot",15000);
+            node.processingNow = true;
+            
+            if (node.inputType === "msg") {
+                try {
+                    // Get the file path from the specified message field
+                    node.filePath = RED.util.getMessageProperty(msg, node.inputField);
+                } 
+                catch(err) {
+                    node.error("Error getting file path from msg." + node.inputField + " : " + err.message);
+                    node_status("couldn't get file path from msg","red","dot",1500);
+                    node_status2("running","blue","ring",1500);
+                    return;
+                }
+                
+                if (!node.filePath || node.filePath === "" || typeof node.filePath !== 'string') {
+                    node.error("The msg." + node.inputField + " should contain a file path");
+                    node_status("file path format is not valid","red","dot",1500);
+                    node_status2("running","blue","ring",1500);
+                    return;
+                }
+            }
+            else { // str
+                node.filePath = node.inputField;
+            }
+
+            if (!fs.existsSync(node.filePath)){
+                node.error("The file path does not exist");
+                node_status("file path does not exist","red","dot",1500);
+                node_status2("not started","grey","ring",1500);
+                return;
+            }
+            
+            node.filePath += "\n";
+            node.transcribeWav.stdin.write(node.filePath);
+            return;
+            
+        }
+        
+        // Retrieve the config node
+        node.voice2JsonConfig = RED.nodes.getNode(config.voice2JsonConfig);
+        node_status2("not started","grey","ring",1);
+        
+        if (node.voice2JsonConfig) {
+            // Use the profile path which has been specified in the config node
+            node.profilePath = node.voice2JsonConfig.profilePath;
+            //check path
+            if (!fs.existsSync(node.profilePath)){
+                node.error("Profile path doesn't exist. Please check the profile path");
+                node_status("profile path error","red","dot",1500);
+                return;
+            }
+        }
+
+        node.on("input", function(msg) {
+        
+            let inputMsg = RED.util.getMessageProperty(msg, node.inputField);
+            
+            if(inputMsg === "start"){
+                if(node.transcribeWav){
+                    node.warn("restarting");
+                    node.transcribeWav.kill();
+                    delete node.transcribeWav;
+                } else {
+                    node.warn("starting");
+                }
+                spawnTranscribe(msg);
+                return;
+            } else if(inputMsg === "stop" && node.transcribeWav){
+                node.transcribeWav.kill();
+                delete node.transcribeWav;
+                return;
+            } 
+            
+            if(node.processingNow == true) {
+                let warnmsg = "Ignoring input message because the previous message is not processed yet";
+                console.log(warnmsg);
+                node.warn(warnmsg);
+                return;
+            }
+            
+            writeStdin(msg);  
+            return;           
+            
         });
         
         node.on("close",function() {
@@ -146,9 +205,16 @@
                node.statusTimer = false;
                node.status({});
             }
-            if(node.childProcess) {
-                node.childProcess.kill();
-                delete node.childProcess;
+            
+            if(node.statusTimer2 !== false){
+               clearTimeout(node.statusTimer2);
+               node.statusTimer2 = false;
+               node.status({});
+            }
+            
+            if(node.transcribeWav) {
+                node.transcribeWav.kill();
+                delete node.transcribeWav;
             }
         });
     }
